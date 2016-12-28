4c3a9d68d091945f79a61d7af473e69571fc77ae
giulianna <j.bustinza42@gmail.com>
2016-12-28 14:02:07 -0500

    añadiendo enlaces

--- main.js
+++ main.js
@@ -3,19 +3,21 @@ function calcular()
 {
     var b = document.getElementById("b").value;
 	var a = document.getElementById("a").value;
+    var negative =parseInt(document.getElementById("negat")).value;
     var salida = document.getElementById("salida");
    
-    if (a<0 && b<0)
+    if ((a<0 && b>0) || (a>0 && b<0) && negative==0)
     {
         salida.innerHTML="1";
     }
-    if (a<0 || b<0)
-    {
-        salida.innerHTML="1";   
+     else
+        {
+        if ((negative==1)&&(a<0 && b<0))
+             salida.innerHTML="1";
+        else 
+             salida.innerHTML="0";
     }
-    else {
-        salida.innerHTML="0";
-    }
+    
 
 }
 
